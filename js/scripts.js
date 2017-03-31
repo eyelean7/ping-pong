85d15c9329bd2a3173350019d0a6c6b012f3bd93
Ilene Gorski <ilene.gorski@gmail.com>
2017-03-31 09:33:29 -0700

    Add backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 //Back-End
 function pingPong(userNumber) {
-  numArray = [1, 2, 3];
+  numArray = [];
   for (var i=1; i<=userNumber; i++) {
-
-    // if (i%15 === 0) {
-    //   numArray.push("ping-pong");
-    // }
-    // else if (i%5 === 0) {
-    //   numArray.push("pong");
-    // }
-    // else if (i%3 === 0) {
-    //   numArray.push("ping");
-    // }
-    // else {
-    //   numArray.push(i);
-    // }
+    if (i%15 === 0) {
+      numArray.push("ping-pong");
+    }
+    else if (i%5 === 0) {
+      numArray.push("pong");
+    }
+    else if (i%3 === 0) {
+      numArray.push("ping");
+    }
+    else {
+      numArray.push(i);
+    }
   }
-  return numArray;
+  return numArray.join(" ");
 }
 
 //User Interface
